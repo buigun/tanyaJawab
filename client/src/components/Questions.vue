226f50dde998bd584d5ce4ed4593f66f60ef5f35
anandapuja <anandapujawandra@gmail.com>
2020-04-16 22:27:11 +0700

    Mockup clear

---
+++ client/src/components/Questions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-container">
+    <div class="title-question">
+      <h1>Answer this questions please</h1>
+    </div>
+    <div class="the-question mt-5">
+      <h3>Pertanyaannya apakah kamu oke?</h3>
+    </div>
+    <div class="choose-one mt-5">
+      <button class="btn btn-success percent-50">YES</button>
+      <button class="btn btn-danger percent-50">NO</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Questions',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
---
+++ client/src/components/Scores.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="my-container">
+    <div class="title-question">
+      <img class="mb-4" style="width:100px" src="../assets/piala.png">
+      <h1>Scores</h1>
+    </div>
+    <div>
+      <h4>Congratulations to the winner, see you next time ...</h4>
+    </div>
+    <div class="space-around mt-3 mb-4">
+        <div class="score-round"><p>1</p></div>
+        <div class="score-round"><p>2</p></div>
+        <div class="score-round"><p>3</p></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Scores',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>

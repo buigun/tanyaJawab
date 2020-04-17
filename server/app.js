9b3cec955717a55193e759a8ccd005374ebc62d3
Budi Gunawan <budiindra.g@gmail.com>
2020-04-17 13:42:25 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const {Question} = require('./models')
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
